4472c17c8bebfa93abdbe251ba27b4246b02b768
Orual <orual.nbc@proton.me>
2025-04-22 15:50:18 -0400

    Modifying flake for android build

--- flake.nix
+++ flake.nix
@@ -1,23 +1,62 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+    android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
   };
 
   outputs =
-    { nixpkgs, systems, ... }:
+    { nixpkgs, flake-utils,  android-nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      forAllSystems =
-        function: nixpkgs.lib.genAttrs (import systems) (system: function nixpkgs.legacyPackages.${system});
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
+      };
+      pinnedJDK = pkgs.jdk17;
+      androidBuildToolsVersion = "35.0.0";
+      androidNdkVersion="26.1.10909125";
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        toolsVersion = "26.1.1";
+        cmdLineToolsVersion = "8.0";
+        platformToolsVersion = "35.0.1";
+        buildToolsVersions = [ androidBuildToolsVersion "34.0.0" ];
+        platformVersions = [ "35" ];
+        cmakeVersions = [ "3.10.2" "3.22.1" ];
+        includeNDK = true;
+        ndkVersions = [ androidNdkVersion  ];
+        useGoogleTVAddOns = false;
+      };
+      androidSdk = androidComposition.androidsdk;
     in
+    with pkgs;
     {
-      packages = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./default.nix { };
-      });
-      devShells = forAllSystems (pkgs: {
+      packages = {
+        default = callPackage ./default.nix { };
+      };
+      devShells = {
         default =
-          with pkgs;
-          mkShell {
+          mkShell rec {
+            buildInputs = [
+              # Android
+              pinnedJDK
+              androidSdk
+              pkg-config
+            ];
+
+            JAVA_HOME = pinnedJDK;
+            ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+            ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${androidBuildToolsVersion}/aapt2";
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.libxml2.out}/lib:$LD_LIBRARY_PATH"
+            '';
+
             packages = [
               just
               fastmod
@@ -27,8 +66,10 @@
 
               typescript
               typescript-language-server
+
             ];
+
           };
-      });
-    };
+      };
+    });
 }
