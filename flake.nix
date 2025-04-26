0a361cf687f4b9d15d142bc4cabe74143f200e80
Orual <orual.nbc@proton.me>
2025-04-25 22:36:35 -0400

    got android emulator working
    mostly self-explanatory, `nix run .#create-emulator` to make the right
    emulator, then `nix run .#run-emulator` to run it. then you can `yarn
    android` to build and run the android version. sometimes it seems to
    crash on first startup, click through to expo and connect to the dev
    server again in expo.

--- flake.nix
+++ flake.nix
@@ -27,12 +27,40 @@
         cmake-3-22-1
         ndk-26-1-10909125
         ndk-28-0-13004108
+        system-images-android-35-ext15-google-apis-playstore-x86-64
       ]);
     in
     with pkgs;
     {
       packages = {
         default = callPackage ./default.nix { };
+      };
+      apps = {
+        create-emulator = let
+          emulator-wrapper = pkgs.writeShellScriptBin "emulator-script" ''
+            avdmanager create avd --force --name phone --package 'system-images;android-35-ext15;google_apis_playstore;x86_64'
+          '';
+        in {
+          type = "app";
+          program = "${emulator-wrapper}/bin/emulator-script";
+        };
+        run-emulator = let
+          emulator-wrapper = pkgs.writeShellScriptBin "emulator-script" ''
+            emulator @phone # add other parameters here if you need to
+          '';
+        in {
+          type = "app";
+          program = "${emulator-wrapper}/bin/emulator-script";
+        };
+        # emulator gets cranky on niri bc niri doesn't have xwayland
+        run-emulator-niri = let
+          emulator-wrapper = pkgs.writeShellScriptBin "emulator-script" ''
+            ${pkgs.cage}/bin/cage emulator @phone # add other parameters here if you need to
+          '';
+        in {
+          type = "app";
+          program = "${emulator-wrapper}/bin/emulator-script";
+        };
       };
       devShells = {
         default =
@@ -58,8 +86,12 @@
 
               typescript
               typescript-language-server
-
             ];
+
+            enterShell = ''
+                export PATH="${androidSdk}/bin:$PATH"
+                ${(builtins.readFile "${androidSdk}/nix-support/setup-hook")}
+              '';
 
           };
       };
